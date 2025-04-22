7d5a9ac912e6c427dd066a63e1bd607a7a1b29be
_1ms <106094224+GRX005@users.noreply.github.com>
2025-04-22 18:56:04 +0200

    Init..

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java")
+}
+
+group = "_1ms.sml"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+tasks.named<Jar>("jar") {
+    manifest {
+        // set the Main‑Class attribute
+        attributes["Main-Class"] = "_1ms.sml.Main"
+    }
+    // include all runtime dependencies (unzipped if they are jars)
+    /*from(
+        configurations
+            .runtimeClasspath
+            .get()
+            .map { file ->
+                if (file.isDirectory) file
+                else zipTree(file)
+            }
+    )*/
+}
---
+++ src/main/java/_1ms/sml/Main.java
@@ -0,0 +1,48 @@
+package _1ms.sml;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+//.jar libs in libraries folder, .dlls in natives folder, mc jar next to the launcher., assets in assets folder, assetIndex is
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String currentDir = System.getProperty("user.dir");
+        String javaBin = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
+        String classPath = currentDir + File.separator + "libraries" + File.separator + "*" + File.pathSeparator + currentDir + File.separator + "1.21.5.jar";
+        String nativesPath = currentDir + File.separator + "natives";
+        String gameDir = currentDir + File.separator + "MC";
+        String assetsDir = currentDir + File.separator + "assets";
+        String launcherBrand = "SML";
+
+        ProcessBuilder proc= new ProcessBuilder(
+                javaBin,
+                "-Xms1024M",
+                "-Xmx4096M",
+                "-Djava.library.path=" + nativesPath,
+                "-Djna.tmpdir=" + nativesPath,
+                "-Dorg.lwjgl.system.SharedLibraryExtractPath=" + nativesPath,
+                "-Dio.netty.native.workdir="+nativesPath,
+                "-cp", classPath,
+                "-Xmx2G", "-XX:+UnlockExperimentalVMOptions", "-XX:+UseG1GC", "-XX:G1NewSizePercent=20", "-XX:G1ReservePercent=20", "-XX:MaxGCPauseMillis=50", "-XX:G1HeapRegionSize=32M", //GC
+                "-Dminecraft.launcher.brand=" + launcherBrand,
+                "-Dminecraft.launcher.version=" + "1.0",
+                "-XX:HeapDumpPath=" + "MojangTricksIntelDriversForPerformance_javaw.exe_minecraft.exe.heapdump",
+                "-Xss1M",
+                "net.minecraft.client.main.Main",
+                //"--username", "asd",
+                "--uuid", UUID.randomUUID().toString(),
+                "--clientId", launcherBrand,
+                "--xuid", launcherBrand,
+                "--version", "1.21.5",
+                "--versionType", launcherBrand,
+                "--gameDir", gameDir,
+                "--assetsDir", assetsDir,
+                "--assetIndex", "24",
+                "--accessToken", launcherBrand
+        );
+        proc.inheritIO();
+        proc.start();
+    }
+}
